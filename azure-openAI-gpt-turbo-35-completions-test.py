e4789a5de6c3fedf7b6e1ca173747ce1446356ee
OZGUR GULER <ozgur.guler1@gmail.com>
2023-03-21 22:55:18 +0300

    works

--- azure-openAI-gpt-turbo-35-completions-test.py
+++ azure-openAI-gpt-turbo-35-completions-test.py
@@ -1,8 +1,11 @@
 import requests
 import json
+import os 
 
 # Replace with your API key and endpoint
-subscription_key = 'fc24064d3f5746c28e7dbb49a19a2567'
+#subscription_key = 'fc24064d3f5746c28e7dbb49a19a2567'
+
+subscription_key = os.environ['OPENAI_API_KEY']
 endpoint = 'https://azure-ozguler-techexplorer.openai.azure.com'
 
 # Set headers for the HTTP request
@@ -14,7 +17,7 @@ headers = {
 # Set parameters for the API call
 payload = {
     'prompt': 'Once upon a time...',
-    'model': 'text-davinci-003',
+    'model': 'gpt-35-turbo',
     'temperature': 0.5,
     'max_tokens': 150,
     'top_p': 1,
@@ -22,7 +25,8 @@ payload = {
     'presence_penalty': 0
 }
                               
-response = requests.post( endpoint + '/openai/deployments/text-davinci-003-ozguler/completions?api-version=2022-12-01', headers=headers, json=payload)
+response = requests.post( endpoint + '/openai/deployments/gpt-35-turbo-ozguler/completions?api-version=2022-12-01', 
+                         headers=headers, json=payload)
 
 data = json.loads(response.text)
 generated_text = data['choices'][0]['text']
